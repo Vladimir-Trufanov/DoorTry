edbbc0f3964ada3b9b39f42a25b43802f863609e
Труфанов Владимир Евгеньевич <tve@karelia.ru>
2021-08-03 11:58:27 +0300

    Отладка сообщений при аякс-загрузке изображения

--- www/Pages/SignaPhoto/SignaPhoto.js
+++ www/Pages/SignaPhoto/SignaPhoto.js
@@ -13,22 +13,33 @@
 // ****************************************************************************
 function isLabel($mess,$subs,$Before='~~~',$After='~~~')
 {
-   $Result=false;
-   $Label=makeLabel($subs,$Before,$After);
-   regex=new RegExp($Label,"u");
-   match=regex.exec($mess);
-   //alert('$subs='+$subs+'  $mess='+$mess);
-   alert('match[0]='+match[0]+'  $Label='+$Label);
-   if (match[0]==$Label) $Result=true;
-   return $Result;
+   // Формируем метку
+   let Label=makeLabel($subs,$Before,$After);
+   // Вычисляем результат регулярного выражения
+   regex=new RegExp(Label,"u");
+   let match=regex.exec($mess);
+   // Разбираем результат
+   if (match==null) 
+   {
+      Result=false;
+   }
+   else if (match[0]==Label)
+   {
+      Result=true;
+   }
+   else 
+   {
+      Result=false;
+   }
+   return Result;
 }
 // ****************************************************************************
 // *                  Выделить метку для отправляемого сообщения              *
 // ****************************************************************************
 function makeLabel($subs,$Before='~~~',$After='~~~')
 {
-   $Result=$Before+$subs+$After;
-   return $Result;
+   let Result=$Before+$subs+$After;
+   return Result;
 }
 // ****************************************************************************
 // *     Обработать клик "Загрузить фотографию для наложения подписи"         *
@@ -55,7 +66,7 @@ function clickLoadPic()
    '<input type="submit" value="Загрузить">'+
    '</div>'+ 
    '<div id="result">'+
-   '****'+
+   'Выберите изображение для нанесения подписи!'+
    '</div>'+
    '</form>';
    // Изменяем заголовок 'input file'
@@ -73,7 +84,7 @@ function clickLoadPic()
       //alert('Перед вызовом аякс');
       $.ajax({
          type:'POST',            // тип запроса
-         url: 'ajaLoadPic.php', // скрипт обработчика
+         url: 'ajaLoadPic.php',  // скрипт обработчика
          async: false,
          data: formData,         // данные которые мы передаем
          cache: false,           // по POST отключено, но явно уточняем
@@ -82,23 +93,26 @@ function clickLoadPic()
          // Отмечаем результат выполнения скрипта по аякс-запросу (успешный или нет)
          success:function(data)
          {
-            
-            if (isLabel(data,ajSuccessfully)) printMessage('#result',ajSuccessfully)
-            else printMessage('#result','Ошибка 1991');
-            /*
-            $preg = new RegExp("шаблон","u");
-            alert('1: '+data);
-            alert('2: '+makeLabel(ajSuccessfully));  // 'Успешно!'
-            
-            text = data;
-            $Label=makeLabel(ajSuccessfully);
-            regex = new RegExp($Label);
-            match = regex.exec(text);
-            alert('3: '+match[0]);       
-            isLabel(text,ajSuccessfully)
-            */
-            
-            alert('Успешно!');
+            //console.log(data);
+            //alert(data);
+            // Если "Не установлен массив файлов и не загружены данные"
+            if (isLabel(data,ajNoSetFile)) 
+            {
+               printMessage('#result',ajNoSetFile);
+            }
+            //Если "Файл успешно загружен"
+            else if (isLabel(data,ajSuccessfully)) 
+            {
+               printMessage('#result',ajSuccessfully);
+            }
+            else if (isLabel(data,ajErrBigFile)) 
+            {
+               printMessage('#result',ajErrBigFile);
+            }
+            else 
+            {
+               printMessage('#result',ajErrorisLabel,data);
+            }
          },
          // Отмечаем  неуспешное выполнение аякс-запроса по причине:
          // 1) утерян файл скрипта.
@@ -107,118 +121,96 @@ function clickLoadPic()
             printMessage('#result', 'Утерян файл скрипта!');
          }
       });
-      // alert('После');
+      //alert('После');
    }));
 }
-
-
-// 
-  function printMessage(destination, msg) 
-  {
-    //
-    $(destination).removeClass();
-    if (msg == ajSuccessfully) {
-      $(destination).addClass('alert alert-success').text('Файл успешно загружен.');
-    }
-    else if (msg == 'Большой файл!') {
-      $(destination).addClass('alert alert-danger').text('Ошибка при загрузке, большой размер файла.');
-    }
+// ****************************************************************************
+// *                  Заменить указанный div разметки сообщением              *
+// ****************************************************************************
+function printMessage(destination,msg,mess1='',mess2='') 
+{
+   // Удаляем фрагмент разметки по указанному классу или идентификатору
+   $(destination).removeClass();
+   // Не найдена метка в переданном сообщении
+   if (msg == ajErrorisLabel) {
+      $(destination).addClass('alert-danger').text(msg+': '+mess1+' ['+mess2+']');
+   }
+   // Ошибка при загрузке файла во временное хранилище
+   else if (msg == ajErrTempoFile) {
+      $(destination).addClass('alert-danger').text(msg+'!');
+   }
+   // Загрузите изображение для нанесения подписи
+   else if (msg == ajInfoLoadImg) {
+      $(destination).addClass('alert-info').text(msg+'!');
+   }
+   // Не установлен массив файлов и не загружены данные
+   else if (msg == ajNoSetFile) {
+      $(destination).addClass('alert-danger').text(msg+'!');
+   }
+   // Не загружен файл во временное хранилище
+   else if (msg == ajNoTempoFile) {
+      $(destination).addClass('alert-danger').text(msg+'!');
+   }
+   // Файл успешно загружен
+   else if (msg == ajSuccessfully) {
+      $(destination).addClass('alert-success').text(msg+'!');
+   }
+   else if (msg == ajErrBigFile) {
+      $(destination).addClass('alert-danger').text('Ошибка при загрузке, большой размер файла.');
+   }
     else if (msg == 'Не изображение!') {
-      $(destination).addClass('alert alert-danger').text('Неверный тип файла изображения.');
+      $(destination).addClass('alert-danger').text('Неверный тип файла изображения.');
     }
     else if (msg == 'Ошибка переброса!') {
-      $(destination).addClass('alert alert-danger').text('Ошибка при перемещении файла на сервер.');
+      $(destination).addClass('alert-danger').text('Ошибка при перемещении файла на сервер.');
     }
-    // Выводим сообщение при всех прочих ошибках
-    else {
-      $(destination).addClass('alert alert-danger').text('Произошла ошибка при загрузке файла.');
-    }
- 
-  }
-
-
+   // Выводим сообщение при всех прочих ошибках
+   else {
+      $(destination).addClass('alert alert-danger').text('Неучтенная ошибка при выполнении операции.');
+   }
+}
 // ****************************************************************************
-// *          Обработать разметку страницы, подключить аякс-сценарии          *
-// *                    по завершению загрузки страницы                       *
+// *       Выбрать и скопировать изображение во временное хранилище           *
 // ****************************************************************************
-/*
-$(document).ready(function () 
-{
-  // Изменяем заголовок 'input file'
-  //$('#ipfLoadPic').inputFileText({text: 'Выбрать изображение'});
-  // Выполняем чтение файла изображения во временной файл по $FILES (по клику)
-  $('#ipfLoadPic').change(function(){
-    readImage(this,'parameter');
-  });
-  
-  //
-  $('#frmLoadPic').on('submit',(function(e) {
-    e.preventDefault();
- 
-    var formData = new FormData(this);
- 
-    $.ajax({
-      type:'POST', // Тип запроса
-      url: 'Handler.php', // Скрипт обработчика
-      data: formData, // Данные которые мы передаем
-      cache:false, // В запросах POST отключено по умолчанию, но перестрахуемся
-      contentType: false, // Тип кодирования данных мы задали в форме, это отключим
-      processData: false, // Отключаем, так как передаем файл
-      // Отмечаем результат выполнения скрипта по аякс-запросу (успешный или нет)
-      success:function(data){
-        printMessage('#result', data);
-      },
-      // Отмечаем  неуспешное выполнение аякс-запроса по причине:
-      //   1) утерян файл скрипта.
-      error:function(data){
-        console.log(data);
-        printMessage('#result', 'Утерян файл скрипта!');
-      }
-    });
-  }));
-  
-  
-  //
-  
-});
-*/
-
 function readImage(input,actstr) 
 {
-  // Если выбран и загружен во временное хранилище хотя бы один файл
-  if (input.files && input.files[0]) 
-  {
-    // Трассируем параметры загружаемого файла
-    /*
-    console.log(input.files[0]);
-    console.log(input.files[0].name);
-    console.log(input.files[0].lastModified);
-    console.log(input.files[0].type);
-    console.log(input.files[0].size+' байт');
-    */
-    // Создаем объект чтения содержимого файла, 
-    // хранящиеся на компьютере пользователя
-    // (асинхронно, чтобы не тормозить браузер)
-    var reader = new FileReader();
-    // Прицепляем замену существующего изображения на загруженное
-    // при успешном завершении загрузки страницы
-    reader.onload = function (e) 
-    {
-      $('#pic').attr('src', e.target.result);
-    }
-    // Прицепляем сообщение об ошибке при ошибке загрузки
-    reader.onerror = function(event) 
-    {
-      printMessage('#result', 'Ошибка "ONERROR" в readImage!');
-      // reader.abort(); 
-    };
-    // Запускаем процесс чтения изображения 
-    reader.readAsDataURL(input.files[0]);
-  }
-  else
-  {
-    printMessage('#result', 'Ошибка "NOT INPUT" readImage!');
-  }
+   // Если выбран и загружен во временное хранилище хотя бы один файл
+   if (input.files && input.files[0]) 
+   {
+      // Трассируем параметры загружаемого файла
+      /*
+      console.log(input.files[0]);
+      console.log(input.files[0].name);
+      console.log(input.files[0].lastModified);
+      console.log(input.files[0].type);
+      console.log(input.files[0].size+' байт');
+      */
+      // Создаем объект чтения содержимого файла, 
+      // хранящиеся на компьютере пользователя
+      // (асинхронно, чтобы не тормозить браузер)
+      var reader = new FileReader();
+      // Прицепляем замену существующего изображения на загруженное
+      // при успешном завершении загрузки страницы
+      reader.onload = function (event) 
+      {
+         $('#pic').attr('src', event.target.result);
+         // Напоминаем о дальнейшем шаге "Загрузите изображение для нанесения подписи"
+         printMessage('#result',ajInfoLoadImg);
+      }
+      // Прицепляем сообщение об ошибке загрузки во временное хранилище
+      reader.onerror = function(event) 
+      {
+         printMessage('#result',ajErrTempoFile);
+         reader.abort(); 
+      };
+      // Запускаем процесс чтения изображения 
+      reader.readAsDataURL(input.files[0]);
+   }
+   // Отмечаем, что не загружен файл во временное хранилище
+   else
+   {
+      printMessage('#result',ajNoTempoFile);
+   }
 }
 
 // ********************************************************** SignaPhoto.js ***
--- www/Pages/SignaPhoto/SignaPhotoDef.php
+++ www/Pages/SignaPhoto/SignaPhotoDef.php
@@ -1,10 +1,22 @@
 <?php
 
 // Определения для PHP
-define ("ajSuccessfully", "Успешно"); 
+define ("ajErrBigFile",   "Файл превышает максимальный размер");  
+define ("ajNoSetFile",    "Не установлен массив файлов и не загружены данные");
+define ("ajErrorisLabel", "Не найдена метка в переданном сообщении");  
+define ("ajErrTempoFile", "Ошибка при загрузке файла во временное хранилище");
+define ("ajInfoLoadImg",  "Загрузите изображение для нанесения подписи");     
+define ("ajNoTempoFile",  "Не загружен файл во временное хранилище");
+define ("ajSuccessfully", "Файл успешно загружен");     
 
 // Переменные JavaScript, соответствующие определениям в PHP
 $define=
 '<script>'.
+'ajErrBigFile="'  .ajErrBigFile.  '";'.
+'ajErrorisLabel="'.ajErrorisLabel.'";'.
+'ajErrTempoFile="'.ajErrTempoFile.'";'.
+'ajInfoLoadImg="' .ajInfoLoadImg. '";'.
+'ajNoSetFile="'   .ajNoSetFile.   '";'.
+'ajNoTempoFile="' .ajNoTempoFile. '";'.
 'ajSuccessfully="'.ajSuccessfully.'";'.
 '</script>';
